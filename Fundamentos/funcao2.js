0a6c7227e705b7f4a591b31758390e48e2a71f6e
PC-TRABALHO <paulo.bruno04@hotmail.com>
2019-01-15 08:21:44 -0300

    exemplos de funcoes

---
+++ Fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+// Armaazenando uma funcao em uma variavel 
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Armazenando uma funcao Arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// Retorno Implícito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!!!')
