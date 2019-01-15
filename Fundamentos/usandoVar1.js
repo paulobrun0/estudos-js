70c4e10eba536d1065f0c6fcd48e730556bd8539
PC-TRABALHO <paulo.bruno04@hotmail.com>
2019-01-15 08:34:57 -0300

    Declarando variaveis com var

---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,16 @@
+{
+    {
+        {
+            {
+                var sera = 'Sera???'
+            }
+        }
+    }
+}
+console.log(sera)
+
+function test() {
+    var local = 123
+}
+
+console.log(local)
