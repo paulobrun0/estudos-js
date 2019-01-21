e4261a8e77c4751773827eedb1ba1902fae676b4
PC-TRABALHO <paulo.bruno04@hotmail.com>
2019-01-21 16:24:57 -0300

    Aulas sobre Estruturas de Controle IF, ELSE, ELSE IF e SWITCH

---
+++ Estrutura de Controle/ifElseif.js
@@ -0,0 +1,25 @@
+Number.prototype.entre = function (inicio, fim) {
+    return this >= inicio && this <= fim
+
+}
+
+const imprirResultdo = function(nota) {
+    if  (nota.entre(9, 10)) {
+        console.log('Quadro de Hora')
+    } else if (nota.entre(7, 8.99)){
+        console.log('Aprovado')
+    } else if (nota.entre(4, 6.99)) {
+        console.log('Recuperação')
+    } else if (nota.entre(0, 3.99)) {
+        console.log('Reprovado')
+    } else {
+        console.log('Nota Inválida')
+    }
+}
+
+imprirResultdo(10)
+imprirResultdo(8.5)
+imprirResultdo(6.55)
+imprirResultdo(2)
+imprirResultdo(-1)
+imprirResultdo(11)
